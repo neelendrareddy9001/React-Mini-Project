a993cf7c8419e4c2d2e65bdfd8c10174dd974b2e
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-20 13:09:23 +0200

    Added

--- expensive-tracker/src/components/AddTransition.js
+++ expensive-tracker/src/components/AddTransition.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const AddTransition = () => {
+    const [text, setText] = useState(' ');
+    const [amount, setAmount] = useState(0);
   return (
     <>
         <h3>Add new transition</h3>
         <form>
             <div className='form-control'>
                 <label htmlFor='text'>Teaxt</label>
-                <input type='text' placeholder='Enter text...'/>
+                <input 
+                    type='text'
+                    placeholder='Enter text...'
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                />
             </div>
             <div className='form-control'>
                 <label htmlFor='amount'
                     >Amount <br />
                 </label>
-                <input type='number' placeholder='Enter amount...' />
+                <input 
+                    type='number' 
+                    placeholder='Enter amount...' 
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                />
             </div>
             <button className='btn'>Add transition</button>
         </form>
