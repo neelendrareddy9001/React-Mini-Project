d34df6826c57456cc10ae8561a3df144e02a11ce
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-20 11:50:59 +0200

    Added

--- expensive-tracker/src/App.js
+++ expensive-tracker/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpensive from './components/IncomeExpensive';
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className='container'>
+        <Balance/>
+        <IncomeExpensive/>
+      </div>
     </div>
   );
 }
