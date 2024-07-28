39797332d00922ae9f662e965fbb0d771edab494
neelendrarerddy9001 <neelendrareddy123@gmail.com>
2024-07-28 23:43:46 +0200

    react-scroll, ract-icons tailwindcss packages installed

---
+++ coffee-web/src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <div className='flex flex-row justify-between p-5 lg:px-32 px-5 bg-gradient-to-r from-backgroundColor to-brightColor shadow-[0_3px_10px_rgba(0,0,0,0.2'>
+            <div className='flex flex-row items-center cursor-pointer gap-2'>
+                <span>
+                    <SiCoffeescript />
+                </span>
+                <h1 className='text-xl font-semibold'>CafePulse</h1>
+            </div>
+            <nav className='hidden md:flex flex-row items-center gap-8 text-lg font-medium'>
+                <Link to="home" spy={true} smooth={true} duration={500} className="cursor-pointer">Home</Link>
+                <Link to="menu" spy={true} smooth={true} duration={500} className="cursor-pointer">Menu</Link>
+                <Link to="about" spy={true} smooth={true} duration={500} className="cursor-pointer">About</Link>
+                <Link to="products" spy={true} smooth={true} duration={500} className="cursor-pointer">Products</Link>
+                <Link to="review" spy={true} smooth={true} duration={500} className="cursor-pointer">Contact</Link>
+            </nav>
+            <div>
+                <Button title="Login"></Button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
